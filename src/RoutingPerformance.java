480da1904dbb6a62eedb5ac993c6e91850cbe377
Amal Vivek <a.vivek@student.unsw.edu.au>
2017-09-26 19:36:33 +1000

    update numero uno

--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -1,10 +1,12 @@
+import java.io.*;
+import java.util.*;
 
 public class RoutingPerformance {
 	
 	private String NETWORK_SCHEME;
 	private String ROUTING_SCHEME;
-	private String TOPOLOGY_FILE;
-	private String WORKLOAD_FILE;
+	private File TOPOLOGY_FILE;
+	private File WORKLOAD_FILE;
 	private int PACKET_RATE;
 
 	
@@ -13,12 +15,12 @@ public class RoutingPerformance {
 		
 		this.NETWORK_SCHEME = nETWORK_SCHEME;
 		this.ROUTING_SCHEME = rOUTING_SCHEME;
-		this.TOPOLOGY_FILE = tOPOLOGY_FILE;
-		this.WORKLOAD_FILE = wORKLOAD_FILE;
+		this.TOPOLOGY_FILE = new File(tOPOLOGY_FILE);
+		this.WORKLOAD_FILE = new File(wORKLOAD_FILE);
 		this.PACKET_RATE = Integer.parseInt(pACKET_RATE);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		if (args.length != 5) {
 			System.err.println("Incorrect Usage: <NETWORK_SCHEME> <ROUTING_SCHEME> "
@@ -27,7 +29,12 @@ public class RoutingPerformance {
 		}
 		
 		RoutingPerformance rp = new RoutingPerformance(args[0], args[1], args[2], args[3], args[4]);
+		
+		Scanner read = new Scanner(rp.TOPOLOGY_FILE);
+		read.useDelimiter(" ");
+		while (read.hasNext()) {
+			System.out.println(read.next());
+		}
 
 	}
-
 }

8c3f907a1fdd050db3ad31c197c32e83d097611e
Amal Vivek <avivek99@gmail.com>
2017-10-22 20:49:27 +1100

    updated graph implementation
    changed the constructor in network

--- src/Network.java
+++ src/Network.java
@@ -1,26 +1,41 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Network {
 	
-	private Link[][] n;
+	private Link[][] n = new Link[26][26];
 
-	public Network() {
-		this.n = new Link[26][26];
+	public Network(String fileName) throws FileNotFoundException {
+		Link[][] network = new Link[26][26];
+		Scanner in = new Scanner(new FileReader(fileName));
+		while(in.hasNextLine()) {
+		    String n1 = in.next();
+		    String n2 = in.next();
+		    int delay = Integer.parseInt(in.next());
+		    int capacity = Integer.parseInt(in.next());
+		    network = add(network, n1, n2, delay, capacity);
+		}
+		in.close();
+		this.n = network; 
+		print(this.n);
 	}
 	
-	public void add(String n1, String n2, int delay, int capacity) {
+	private Link[][] add(Link[][] n, String n1, String n2, int delay, int capacity) {
 		Link link = new Link(delay, capacity);
-		this.n[let2Num(n1)][let2Num(n2)] = link;
-		this.n[let2Num(n2)][let2Num(n1)] = link;
+		n[let2Num(n1)][let2Num(n2)] = link;
+		n[let2Num(n2)][let2Num(n1)] = link;
+		return n;
 	}
 	
-	public void print() {
+	private void print(Link[][] n) {
 		for (int i = 0; i < 26; i++) {
-			for (Link node : this.n[i]) {
-				if (node == null) {
+			for (Link link : n[i]) {
+				if (link == null) {
 					System.out.print("[ -1 ,  -1]\t");
 				} else {
-					String d = String.format("%3d", node.getDelay());
-					String c = String.format("%3d", node.getCapacity());
+					String d = String.format("%3d", link.getDelay());
+					String c = String.format("%3d", link.getCapacity());
 					System.out.print("[" + d + " , " + c + "]\t");
 				}
 			}
@@ -94,6 +109,66 @@ public class Network {
 				return 25;
 			default:
 				return -1;
+			
+		}
+	}
+	
+	private int num2Let (int num) {
+		switch (num) {
+			case 0:
+				return 'A';
+			case 1:
+				return 'B';
+			case 2:
+				return 'C';
+			case 3:
+				return 'D';
+			case 4:
+				return 'E';
+			case 5:
+				return 'F';
+			case 6:
+				return 'G';
+			case 7:
+				return 'H';
+			case 8:
+				return 'I';
+			case 9:
+				return 'J';
+			case 10:
+				return 'K';
+			case 11:
+				return 'L';
+			case 12:
+				return 'M';
+			case 13:
+				return 'N';
+			case 14:
+				return 'O';
+			case 15:
+				return 'P';
+			case 16:
+				return 'Q';
+			case 17:
+				return 'R';
+			case 18:
+				return 'S';
+			case 19:
+				return 'T';
+			case 20:
+				return 'U';
+			case 21:
+				return 'V';
+			case 22:
+				return 'W';
+			case 23:
+				return 'X';
+			case 24:
+				return 'Y';
+			case 25:
+				return 'Z';
+			default:
+				return '0';
 			
 		}
 	}

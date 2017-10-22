7520aa5af13739214eb53c356b6887a0cc88367e
Amal Vivek <avivek99@gmail.com>
2017-10-23 00:34:42 +1100

    new packet class

---
+++ src/Packet.java
@@ -0,0 +1,24 @@
+
+public class Packet extends Thread {
+	
+	long time;
+	
+	
+	
+	
+	
+	public Packet() {
+		this.time = System.currentTimeMillis();
+	}
+
+
+
+
+
+	public void run() {
+		
+	}
+	
+	
+		
+}
